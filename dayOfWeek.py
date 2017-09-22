5e0961c338dec35cf9432df23788341ac2591480
Rishav Rout <rout.rishav@gmail.com>
2017-09-21 22:46:54 -0500

    efficiency using %= instead of -=

--- dayOfWeek.py
+++ dayOfWeek.py
@@ -1,13 +1,10 @@
-daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday',
+daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 
               'friday', 'saturday', 'sunday']
 
 def day_of_the_week(day, n):
-    x = int(n)
-    newDay = x + daysOfWeek.index(day)
+    newDay = n + daysOfWeek.index(day)
     while newDay > len(daysOfWeek) - 1:
-        newDay -= 7
-    while newDay < 0:
-        newDay += 7
+        newDay %= 7
     return daysOfWeek[newDay]
 
 again = 'y'
